99a07219259e6da3c25b9c130b671346c5ddb82a
JoacoSM18 <joako.sm.18@gmail.com>
2025-10-16 23:22:24 -0300

    Group Box y Botones y Etiquetas

--- prySilvaMenendez_SP3.AutoTest/frmInicioAutoTest.Designer.cs
+++ prySilvaMenendez_SP3.AutoTest/frmInicioAutoTest.Designer.cs
@@ -28,84 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.mrcTurnos = new System.Windows.Forms.GroupBox();
+            this.mrcEstadisticas = new System.Windows.Forms.GroupBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.lblNumeroTurno = new System.Windows.Forms.Label();
+            this.lblDominio = new System.Windows.Forms.Label();
+            this.lblAño = new System.Windows.Forms.Label();
+            this.lblTitular = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblAñoAntiguedad = new System.Windows.Forms.Label();
+            this.lblCantidadDominio = new System.Windows.Forms.Label();
+            this.mrcTurnos.SuspendLayout();
+            this.mrcEstadisticas.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // mrcTurnos
             // 
-            this.groupBox1.Location = new System.Drawing.Point(12, 37);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(437, 172);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.mrcTurnos.Controls.Add(this.lblTitular);
+            this.mrcTurnos.Controls.Add(this.lblAño);
+            this.mrcTurnos.Controls.Add(this.lblDominio);
+            this.mrcTurnos.Controls.Add(this.lblNumeroTurno);
+            this.mrcTurnos.Location = new System.Drawing.Point(12, 37);
+            this.mrcTurnos.Name = "mrcTurnos";
+            this.mrcTurnos.Size = new System.Drawing.Size(437, 274);
+            this.mrcTurnos.TabIndex = 0;
+            this.mrcTurnos.TabStop = false;
+            this.mrcTurnos.Text = "groupBox1";
             // 
-            // groupBox2
+            // mrcEstadisticas
             // 
-            this.groupBox2.Location = new System.Drawing.Point(13, 243);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(435, 173);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.mrcEstadisticas.Controls.Add(this.lblCantidadDominio);
+            this.mrcEstadisticas.Controls.Add(this.lblAñoAntiguedad);
+            this.mrcEstadisticas.Controls.Add(this.lblCantidad);
+            this.mrcEstadisticas.Location = new System.Drawing.Point(12, 336);
+            this.mrcEstadisticas.Name = "mrcEstadisticas";
+            this.mrcEstadisticas.Size = new System.Drawing.Size(435, 218);
+            this.mrcEstadisticas.TabIndex = 1;
+            this.mrcEstadisticas.TabStop = false;
+            this.mrcEstadisticas.Text = "groupBox2";
             // 
-            // button1
+            // btnRegistrar
             // 
-            this.button1.Location = new System.Drawing.Point(509, 37);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 42);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Location = new System.Drawing.Point(509, 37);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(154, 42);
+            this.btnRegistrar.TabIndex = 2;
+            this.btnRegistrar.Text = "button1";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnSalir
             // 
-            this.button2.Location = new System.Drawing.Point(509, 98);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 41);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSalir.Location = new System.Drawing.Point(509, 98);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(150, 41);
+            this.btnSalir.TabIndex = 3;
+            this.btnSalir.Text = "button2";
+            this.btnSalir.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnConsultar
             // 
-            this.button3.Location = new System.Drawing.Point(509, 243);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(150, 41);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnConsultar.Location = new System.Drawing.Point(509, 243);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(150, 41);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "button3";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // lblNumeroTurno
+            // 
+            this.lblNumeroTurno.AutoSize = true;
+            this.lblNumeroTurno.Location = new System.Drawing.Point(23, 46);
+            this.lblNumeroTurno.Name = "lblNumeroTurno";
+            this.lblNumeroTurno.Size = new System.Drawing.Size(51, 20);
+            this.lblNumeroTurno.TabIndex = 0;
+            this.lblNumeroTurno.Text = "label1";
+            // 
+            // lblDominio
+            // 
+            this.lblDominio.AutoSize = true;
+            this.lblDominio.Location = new System.Drawing.Point(23, 110);
+            this.lblDominio.Name = "lblDominio";
+            this.lblDominio.Size = new System.Drawing.Size(51, 20);
+            this.lblDominio.TabIndex = 1;
+            this.lblDominio.Text = "label2";
+            // 
+            // lblAño
+            // 
+            this.lblAño.AutoSize = true;
+            this.lblAño.Location = new System.Drawing.Point(23, 169);
+            this.lblAño.Name = "lblAño";
+            this.lblAño.Size = new System.Drawing.Size(51, 20);
+            this.lblAño.TabIndex = 2;
+            this.lblAño.Text = "label1";
+            // 
+            // lblTitular
+            // 
+            this.lblTitular.AutoSize = true;
+            this.lblTitular.Location = new System.Drawing.Point(23, 227);
+            this.lblTitular.Name = "lblTitular";
+            this.lblTitular.Size = new System.Drawing.Size(51, 20);
+            this.lblTitular.TabIndex = 3;
+            this.lblTitular.Text = "label2";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(23, 50);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(51, 20);
+            this.lblCantidad.TabIndex = 0;
+            this.lblCantidad.Text = "label1";
+            // 
+            // lblAñoAntiguedad
+            // 
+            this.lblAñoAntiguedad.AutoSize = true;
+            this.lblAñoAntiguedad.Location = new System.Drawing.Point(23, 116);
+            this.lblAñoAntiguedad.Name = "lblAñoAntiguedad";
+            this.lblAñoAntiguedad.Size = new System.Drawing.Size(51, 20);
+            this.lblAñoAntiguedad.TabIndex = 1;
+            this.lblAñoAntiguedad.Text = "label2";
+            // 
+            // lblCantidadDominio
+            // 
+            this.lblCantidadDominio.AutoSize = true;
+            this.lblCantidadDominio.Location = new System.Drawing.Point(23, 174);
+            this.lblCantidadDominio.Name = "lblCantidadDominio";
+            this.lblCantidadDominio.Size = new System.Drawing.Size(51, 20);
+            this.lblCantidadDominio.TabIndex = 2;
+            this.lblCantidadDominio.Text = "label3";
             // 
             // frmInicioAutoTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(716, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(716, 595);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.mrcEstadisticas);
+            this.Controls.Add(this.mrcTurnos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "frmInicioAutoTest";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Auto-Test";
             this.Load += new System.EventHandler(this.frmInicioAutoTest_Load);
+            this.mrcTurnos.ResumeLayout(false);
+            this.mrcTurnos.PerformLayout();
+            this.mrcEstadisticas.ResumeLayout(false);
+            this.mrcEstadisticas.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox mrcTurnos;
+        private System.Windows.Forms.GroupBox mrcEstadisticas;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label lblDominio;
+        private System.Windows.Forms.Label lblNumeroTurno;
+        private System.Windows.Forms.Label lblTitular;
+        private System.Windows.Forms.Label lblAño;
+        private System.Windows.Forms.Label lblCantidadDominio;
+        private System.Windows.Forms.Label lblAñoAntiguedad;
+        private System.Windows.Forms.Label lblCantidad;
     }
 }
 
