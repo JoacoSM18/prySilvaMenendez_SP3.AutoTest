cfff86e5fa74df4811a75850135d2c622b7c1ead
JoacoSM18 <joako.sm.18@gmail.com>
2025-10-22 12:29:07 -0300

    Iconos

--- prySilvaMenendez_SP3.AutoTest/frmInicioAutoTest.Designer.cs
+++ prySilvaMenendez_SP3.AutoTest/frmInicioAutoTest.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInicioAutoTest));
             this.mrcTurnos = new System.Windows.Forms.GroupBox();
             this.mtbNumeroTurno = new System.Windows.Forms.MaskedTextBox();
             this.txtDominio = new System.Windows.Forms.TextBox();
@@ -286,6 +287,7 @@
             this.Controls.Add(this.mrcEstadisticas);
             this.Controls.Add(this.mrcTurnos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmInicioAutoTest";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
