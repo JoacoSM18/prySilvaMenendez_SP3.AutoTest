1d55cdfb8a16856ea4ca0034c3c642901368c5c1
JoacoSM18 <joako.sm.18@gmail.com>
2025-11-02 21:39:07 -0300

    Primer GroupBox Listo, Falta Group Box Estadisticas

--- prySilvaMenendez_SP3.AutoTest/frmInicioAutoTest.Designer.cs
+++ prySilvaMenendez_SP3.AutoTest/frmInicioAutoTest.Designer.cs
@@ -39,9 +39,6 @@
             this.lblDominio = new System.Windows.Forms.Label();
             this.lblNumeroTurno = new System.Windows.Forms.Label();
             this.mrcEstadisticas = new System.Windows.Forms.GroupBox();
-            this.mtbCantidadDominio = new System.Windows.Forms.MaskedTextBox();
-            this.mtbAñoAntiguedad = new System.Windows.Forms.MaskedTextBox();
-            this.mtbCantidadTurnos = new System.Windows.Forms.MaskedTextBox();
             this.lblCantidadDominio = new System.Windows.Forms.Label();
             this.lblAñoAntiguedad = new System.Windows.Forms.Label();
             this.lblCantidadTurnos = new System.Windows.Forms.Label();
@@ -63,41 +60,36 @@
             this.mrcTurnos.Controls.Add(this.lblAño);
             this.mrcTurnos.Controls.Add(this.lblDominio);
             this.mrcTurnos.Controls.Add(this.lblNumeroTurno);
-            this.mrcTurnos.Location = new System.Drawing.Point(8, 14);
-            this.mrcTurnos.Margin = new System.Windows.Forms.Padding(2);
+            this.mrcTurnos.Location = new System.Drawing.Point(12, 22);
             this.mrcTurnos.Name = "mrcTurnos";
-            this.mrcTurnos.Padding = new System.Windows.Forms.Padding(2);
-            this.mrcTurnos.Size = new System.Drawing.Size(327, 178);
+            this.mrcTurnos.Size = new System.Drawing.Size(490, 274);
             this.mrcTurnos.TabIndex = 0;
             this.mrcTurnos.TabStop = false;
             this.mrcTurnos.Text = "Registro de Turnos";
             // 
             // mtbNumeroTurno
             // 
-            this.mtbNumeroTurno.Location = new System.Drawing.Point(123, 30);
-            this.mtbNumeroTurno.Margin = new System.Windows.Forms.Padding(2);
+            this.mtbNumeroTurno.Location = new System.Drawing.Point(184, 46);
             this.mtbNumeroTurno.Mask = "99999";
             this.mtbNumeroTurno.Name = "mtbNumeroTurno";
-            this.mtbNumeroTurno.Size = new System.Drawing.Size(68, 20);
+            this.mtbNumeroTurno.Size = new System.Drawing.Size(100, 26);
             this.mtbNumeroTurno.TabIndex = 0;
             this.mtbNumeroTurno.ValidatingType = typeof(int);
             this.mtbNumeroTurno.Click += new System.EventHandler(this.mtbNumeroTurno_Click);
             // 
             // txtDominio
             // 
-            this.txtDominio.Location = new System.Drawing.Point(123, 72);
-            this.txtDominio.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDominio.Location = new System.Drawing.Point(184, 111);
             this.txtDominio.MaxLength = 7;
             this.txtDominio.Name = "txtDominio";
-            this.txtDominio.Size = new System.Drawing.Size(68, 20);
+            this.txtDominio.Size = new System.Drawing.Size(100, 26);
             this.txtDominio.TabIndex = 1;
             this.txtDominio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDominio_KeyPress);
             this.txtDominio.Validating += new System.ComponentModel.CancelEventHandler(this.txtDominio_Validating);
             // 
             // numAño
             // 
-            this.numAño.Location = new System.Drawing.Point(123, 106);
-            this.numAño.Margin = new System.Windows.Forms.Padding(2);
+            this.numAño.Location = new System.Drawing.Point(184, 163);
             this.numAño.Maximum = new decimal(new int[] {
             2025,
             0,
@@ -109,7 +101,7 @@
             0,
             0});
             this.numAño.Name = "numAño";
-            this.numAño.Size = new System.Drawing.Size(51, 20);
+            this.numAño.Size = new System.Drawing.Size(76, 26);
             this.numAño.TabIndex = 2;
             this.numAño.Value = new decimal(new int[] {
             2025,
@@ -121,142 +113,94 @@
             // 
             // txtTitular
             // 
-            this.txtTitular.Location = new System.Drawing.Point(123, 144);
-            this.txtTitular.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTitular.Location = new System.Drawing.Point(184, 222);
             this.txtTitular.MaxLength = 30;
             this.txtTitular.Name = "txtTitular";
-            this.txtTitular.Size = new System.Drawing.Size(179, 20);
+            this.txtTitular.Size = new System.Drawing.Size(266, 26);
             this.txtTitular.TabIndex = 3;
             this.txtTitular.Validating += new System.ComponentModel.CancelEventHandler(this.txtTitular_Validating);
             // 
             // lblTitular
             // 
             this.lblTitular.AutoSize = true;
-            this.lblTitular.Location = new System.Drawing.Point(15, 148);
-            this.lblTitular.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitular.Location = new System.Drawing.Point(22, 228);
             this.lblTitular.Name = "lblTitular";
-            this.lblTitular.Size = new System.Drawing.Size(36, 13);
+            this.lblTitular.Size = new System.Drawing.Size(52, 20);
             this.lblTitular.TabIndex = 3;
             this.lblTitular.Text = "Titular";
             // 
             // lblAño
             // 
             this.lblAño.AutoSize = true;
-            this.lblAño.Location = new System.Drawing.Point(15, 110);
-            this.lblAño.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAño.Location = new System.Drawing.Point(22, 169);
             this.lblAño.Name = "lblAño";
-            this.lblAño.Size = new System.Drawing.Size(99, 13);
+            this.lblAño.Size = new System.Drawing.Size(146, 20);
             this.lblAño.TabIndex = 2;
             this.lblAño.Text = "Año de Fabricación";
             // 
             // lblDominio
             // 
             this.lblDominio.AutoSize = true;
-            this.lblDominio.Location = new System.Drawing.Point(15, 72);
-            this.lblDominio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDominio.Location = new System.Drawing.Point(22, 111);
             this.lblDominio.Name = "lblDominio";
-            this.lblDominio.Size = new System.Drawing.Size(45, 13);
+            this.lblDominio.Size = new System.Drawing.Size(67, 20);
             this.lblDominio.TabIndex = 1;
             this.lblDominio.Text = "Dominio";
             // 
             // lblNumeroTurno
             // 
             this.lblNumeroTurno.AutoSize = true;
-            this.lblNumeroTurno.Location = new System.Drawing.Point(15, 30);
-            this.lblNumeroTurno.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNumeroTurno.Location = new System.Drawing.Point(22, 46);
             this.lblNumeroTurno.Name = "lblNumeroTurno";
-            this.lblNumeroTurno.Size = new System.Drawing.Size(93, 13);
+            this.lblNumeroTurno.Size = new System.Drawing.Size(136, 20);
             this.lblNumeroTurno.TabIndex = 0;
             this.lblNumeroTurno.Text = "Número de Turno ";
             // 
             // mrcEstadisticas
             // 
-            this.mrcEstadisticas.Controls.Add(this.mtbCantidadDominio);
-            this.mrcEstadisticas.Controls.Add(this.mtbAñoAntiguedad);
-            this.mrcEstadisticas.Controls.Add(this.mtbCantidadTurnos);
             this.mrcEstadisticas.Controls.Add(this.lblCantidadDominio);
             this.mrcEstadisticas.Controls.Add(this.lblAñoAntiguedad);
             this.mrcEstadisticas.Controls.Add(this.lblCantidadTurnos);
-            this.mrcEstadisticas.Location = new System.Drawing.Point(8, 218);
-            this.mrcEstadisticas.Margin = new System.Windows.Forms.Padding(2);
+            this.mrcEstadisticas.Location = new System.Drawing.Point(12, 335);
             this.mrcEstadisticas.Name = "mrcEstadisticas";
-            this.mrcEstadisticas.Padding = new System.Windows.Forms.Padding(2);
-            this.mrcEstadisticas.Size = new System.Drawing.Size(327, 142);
+            this.mrcEstadisticas.Size = new System.Drawing.Size(490, 218);
             this.mrcEstadisticas.TabIndex = 1;
             this.mrcEstadisticas.TabStop = false;
             this.mrcEstadisticas.Text = "Estadisticas";
             // 
-            // mtbCantidadDominio
-            // 
-            this.mtbCantidadDominio.Location = new System.Drawing.Point(213, 109);
-            this.mtbCantidadDominio.Margin = new System.Windows.Forms.Padding(2);
-            this.mtbCantidadDominio.Mask = "99999";
-            this.mtbCantidadDominio.Name = "mtbCantidadDominio";
-            this.mtbCantidadDominio.Size = new System.Drawing.Size(53, 20);
-            this.mtbCantidadDominio.TabIndex = 6;
-            this.mtbCantidadDominio.ValidatingType = typeof(int);
-            this.mtbCantidadDominio.Click += new System.EventHandler(this.mtbCantidadDominio_Click);
-            // 
-            // mtbAñoAntiguedad
-            // 
-            this.mtbAñoAntiguedad.Location = new System.Drawing.Point(213, 68);
-            this.mtbAñoAntiguedad.Margin = new System.Windows.Forms.Padding(2);
-            this.mtbAñoAntiguedad.Mask = "99999";
-            this.mtbAñoAntiguedad.Name = "mtbAñoAntiguedad";
-            this.mtbAñoAntiguedad.Size = new System.Drawing.Size(53, 20);
-            this.mtbAñoAntiguedad.TabIndex = 5;
-            this.mtbAñoAntiguedad.ValidatingType = typeof(int);
-            this.mtbAñoAntiguedad.Click += new System.EventHandler(this.mtbAñoAntiguedad_Click);
-            // 
-            // mtbCantidadTurnos
-            // 
-            this.mtbCantidadTurnos.Location = new System.Drawing.Point(213, 27);
-            this.mtbCantidadTurnos.Margin = new System.Windows.Forms.Padding(2);
-            this.mtbCantidadTurnos.Mask = "99999";
-            this.mtbCantidadTurnos.Name = "mtbCantidadTurnos";
-            this.mtbCantidadTurnos.Size = new System.Drawing.Size(53, 20);
-            this.mtbCantidadTurnos.TabIndex = 4;
-            this.mtbCantidadTurnos.ValidatingType = typeof(int);
-            this.mtbCantidadTurnos.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtbCantidadTurnos_MaskInputRejected);
-            this.mtbCantidadTurnos.Click += new System.EventHandler(this.mtbCantidadTurnos_Click);
-            // 
             // lblCantidadDominio
             // 
             this.lblCantidadDominio.AutoSize = true;
-            this.lblCantidadDominio.Location = new System.Drawing.Point(15, 113);
-            this.lblCantidadDominio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCantidadDominio.Location = new System.Drawing.Point(22, 174);
             this.lblCantidadDominio.Name = "lblCantidadDominio";
-            this.lblCantidadDominio.Size = new System.Drawing.Size(189, 13);
+            this.lblCantidadDominio.Size = new System.Drawing.Size(282, 20);
             this.lblCantidadDominio.TabIndex = 2;
             this.lblCantidadDominio.Text = "Cantidad con Dominio de 6 Caracteres";
             // 
             // lblAñoAntiguedad
             // 
             this.lblAñoAntiguedad.AutoSize = true;
-            this.lblAñoAntiguedad.Location = new System.Drawing.Point(15, 72);
-            this.lblAñoAntiguedad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAñoAntiguedad.Location = new System.Drawing.Point(22, 111);
             this.lblAñoAntiguedad.Name = "lblAñoAntiguedad";
-            this.lblAñoAntiguedad.Size = new System.Drawing.Size(105, 13);
+            this.lblAñoAntiguedad.Size = new System.Drawing.Size(156, 20);
             this.lblAñoAntiguedad.TabIndex = 1;
             this.lblAñoAntiguedad.Text = "Año del Más Antiguo";
             // 
             // lblCantidadTurnos
             // 
             this.lblCantidadTurnos.AutoSize = true;
-            this.lblCantidadTurnos.Location = new System.Drawing.Point(15, 31);
-            this.lblCantidadTurnos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblCantidadTurnos.Location = new System.Drawing.Point(22, 48);
             this.lblCantidadTurnos.Name = "lblCantidadTurnos";
-            this.lblCantidadTurnos.Size = new System.Drawing.Size(100, 13);
+            this.lblCantidadTurnos.Size = new System.Drawing.Size(148, 20);
             this.lblCantidadTurnos.TabIndex = 0;
             this.lblCantidadTurnos.Text = "Cantidad de Turnos";
             // 
             // btnRegistrar
             // 
             this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRegistrar.Location = new System.Drawing.Point(350, 14);
-            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRegistrar.Location = new System.Drawing.Point(525, 22);
             this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(87, 27);
+            this.btnRegistrar.Size = new System.Drawing.Size(130, 42);
             this.btnRegistrar.TabIndex = 7;
             this.btnRegistrar.Text = "Registrar";
             this.btnRegistrar.UseVisualStyleBackColor = true;
@@ -265,10 +209,9 @@
             // btnSalir
             // 
             this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(350, 64);
-            this.btnSalir.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSalir.Location = new System.Drawing.Point(525, 98);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(87, 27);
+            this.btnSalir.Size = new System.Drawing.Size(130, 42);
             this.btnSalir.TabIndex = 8;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -277,20 +220,19 @@
             // btnConsultar
             // 
             this.btnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConsultar.Location = new System.Drawing.Point(350, 218);
-            this.btnConsultar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConsultar.Location = new System.Drawing.Point(525, 335);
             this.btnConsultar.Name = "btnConsultar";
-            this.btnConsultar.Size = new System.Drawing.Size(87, 29);
+            this.btnConsultar.Size = new System.Drawing.Size(130, 45);
             this.btnConsultar.TabIndex = 9;
             this.btnConsultar.Text = "Consultar";
             this.btnConsultar.UseVisualStyleBackColor = true;
             // 
             // frmInicioAutoTest
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Bisque;
-            this.ClientSize = new System.Drawing.Size(458, 387);
+            this.ClientSize = new System.Drawing.Size(687, 595);
             this.Controls.Add(this.btnConsultar);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnRegistrar);
@@ -298,7 +240,6 @@
             this.Controls.Add(this.mrcTurnos);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmInicioAutoTest";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Auto-Test";
@@ -327,9 +268,6 @@
         private System.Windows.Forms.Label lblAñoAntiguedad;
         private System.Windows.Forms.Label lblCantidadTurnos;
         private System.Windows.Forms.TextBox txtTitular;
-        private System.Windows.Forms.MaskedTextBox mtbCantidadDominio;
-        private System.Windows.Forms.MaskedTextBox mtbAñoAntiguedad;
-        private System.Windows.Forms.MaskedTextBox mtbCantidadTurnos;
         private System.Windows.Forms.NumericUpDown numAño;
         private System.Windows.Forms.TextBox txtDominio;
         private System.Windows.Forms.MaskedTextBox mtbNumeroTurno;
--- prySilvaMenendez_SP3.AutoTest/frmInicioAutoTest.cs
+++ prySilvaMenendez_SP3.AutoTest/frmInicioAutoTest.cs
@@ -12,14 +12,27 @@ namespace prySilvaMenendez_SP3.AutoTest
 {
     public partial class frmInicioAutoTest : Form
     {
+        struct Turno
+        {
+            public int numeroturno;
+            public string dominio;
+            public int añofabricacion;
+            public string titular;
+        }
+
+        int indice = 0;
+        Turno[] vecTurnos = new Turno[100];
+
         public frmInicioAutoTest()
         {
             InitializeComponent();
         }
+
         private void frmInicioAutoTest_Load(object sender, EventArgs e)
         {
             txtDominio.CharacterCasing = CharacterCasing.Upper;
         }
+
         private void txtDominio_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsControl(e.KeyChar) && !char.IsLetterOrDigit(e.KeyChar))
@@ -27,20 +40,7 @@ namespace prySilvaMenendez_SP3.AutoTest
                 e.Handled = true;
             }
         }
-        private void btnRegistrar_Click(object sender, EventArgs e)
-        {
-            if (txtDominio.Text.Trim().Length >= 6 &&
-                txtTitular.Text.Trim().Length >= 2 &&
-                mtbNumeroTurno.Text.Trim() != "" &&
-                numAño.Value > 0)
-            {
-                MessageBox.Show("Registro Exitoso", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-            else
-            {
-                MessageBox.Show("Faltan Datos o Hay Campos Inválidos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-        }
+
         private void txtDominio_Validating(object sender, CancelEventArgs e)
         {
             if (txtDominio.Text.Length < 6)
@@ -49,10 +49,7 @@ namespace prySilvaMenendez_SP3.AutoTest
                 e.Cancel = true;
             }
         }
-        private void mtbNumeroTurno_Click(object sender, EventArgs e)
-        {
-            mtbNumeroTurno.SelectionStart = 0;
-        }
+
         private void txtTitular_Validating(object sender, CancelEventArgs e)
         {
             if (txtTitular.Text.Trim().Length < 2)
@@ -61,38 +58,61 @@ namespace prySilvaMenendez_SP3.AutoTest
                 e.Cancel = true;
             }
         }
+
+        private void mtbNumeroTurno_Click(object sender, EventArgs e)
+        {
+            mtbNumeroTurno.SelectionStart = 0;
+        }
+
         private void numAño_Click(object sender, EventArgs e)
         {
             numAño.Select(0, 0);
         }
+
         private void numAño_KeyPress(object sender, KeyPressEventArgs e)
         {
             e.Handled = true;
         }
 
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if (txtDominio.Text.Trim().Length >= 6 &&
+                txtTitular.Text.Trim().Length >= 2 &&
+                mtbNumeroTurno.Text.Trim() != "" &&
+                numAño.Value > 0)
+            {
+                if (!int.TryParse(mtbNumeroTurno.Text, out int numeroTurno))
+                {
+                    MessageBox.Show("El Número de Turno Debe ser un Número Entero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    mtbNumeroTurno.Focus();
+                    return;
+                }
+
+                for (int i = 0; i < indice; i++)
+                {
+                    if (vecTurnos[i].numeroturno == numeroTurno)
+                    {
+                        MessageBox.Show("El Número de Turno Ya Existe. Ingrese Otro.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        mtbNumeroTurno.Focus();
+                        return;
+                    }
+                }
+                vecTurnos[indice].numeroturno = numeroTurno;
+                vecTurnos[indice].dominio = txtDominio.Text.Trim().ToUpper();
+                vecTurnos[indice].añofabricacion = (int)numAño.Value;
+                vecTurnos[indice].titular = txtTitular.Text.Trim();
+                indice++;
+                MessageBox.Show("Registro Exitoso", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtDominio.Clear();
+                txtTitular.Clear();
+                mtbNumeroTurno.Clear();
+                numAño.Value = 2025;
+                mtbNumeroTurno.Focus();
+            }
+        }
         private void btnSalir_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-        private void mtbCantidadTurnos_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
-        {
-
-        }
-
-        private void mtbCantidadTurnos_Click(object sender, EventArgs e)
-        {
-            mtbCantidadTurnos.SelectionStart = 0;
-        }
-
-        private void mtbAñoAntiguedad_Click(object sender, EventArgs e)
-        {
-            mtbAñoAntiguedad.SelectionStart = 0;
-        }
-
-        private void mtbCantidadDominio_Click(object sender, EventArgs e)
-        {
-            mtbCantidadDominio.SelectionStart = 0;
         }
     }
 }
